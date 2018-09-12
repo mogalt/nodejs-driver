ce822c88fc8ebc68fb3495c34891318a5c384512
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2018-09-12 16:10:57 +0200

    Integration tests for bigint data type

--- test/integration/short/client-execute-tests.js
+++ test/integration/short/client-execute-tests.js
@@ -10,6 +10,7 @@ const utils = require('../../../lib/utils.js');
 const errors = require('../../../lib/errors.js');
 const vit = helper.vit;
 const vdescribe = helper.vdescribe;
+const bigIntTests = require('./bigint-tests');
 
 describe('Client', function () {
   this.timeout(120000);
@@ -1033,6 +1034,8 @@ describe('Client', function () {
         });
       });
     });
+
+    bigIntTests(keyspace, false);
   });
 });
 
